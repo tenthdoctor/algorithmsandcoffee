118f48c28bff4fbec63f0c6f4c63175e6f4b5683
The Doctor <doctorandabox@gmail.com>
2016-10-10 20:58:37 +0530

    Binary Search

---
+++ com/probablycorrect/algorithms/java/BinarySearch.java
@@ -0,0 +1,12 @@
+package com.probablycorrect.algorithms.java;
+
+/*
+*
+* Compilation:  javac BinarySearch.java
+* Execution:    java BinarySearch whitelist.txt < input.txt
+* Dependencies: In.java StdIn.java StdOut.java
+* Data files: data/
+*
+* % java BinarySearch tinyW.txt < tinyT.txt
+* % java BinarySearch largeW.txt < largeT.txt | more
+* */
